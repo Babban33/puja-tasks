e89fa1940a2605d1b5c75b1b839a8bff78cdf84a
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-24 13:17:32 +0530

    added header

---
+++ app/taskone/page.tsx
@@ -0,0 +1,12 @@
+export default function TaskOne(){
+    return(
+        <div className="max-w-md mx-auto p-4 min-h-screen bg-gray-50">
+            <header className="my-6">
+                <div className="text-sm text-gray-900">Sun, 1 March 2022</div>
+                <h1 className="text-2xl font-semibold text-gray-900">
+                Hello, <span className="text-orange-500">Susy!</span>
+                </h1>
+            </header>
+        </div>
+    )
+}
