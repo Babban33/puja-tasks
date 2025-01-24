5f8ecab16a1211c1662fcad1a7895e01f62572c1
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-24 13:29:21 +0530

    completed progess section

--- app/taskone/page.tsx
+++ app/taskone/page.tsx
@@ -9,13 +9,15 @@ export default function TaskOne(){
             </header>
 
             <div className="relative mb-8 p-6 bg-gradient-to-br from-orange-500 to-orange-400 text-white rounded-xl overflow-hidden shadow-md">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                <div className="text-xl font-medium">
-                    2 of 3 habits
-                    <br />
-                    completed today!
+                <div className="flex items-center gap-4 mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <div className="text-xl font-medium">
+                        2 of 3 habits
+                        <br />
+                        completed today!
+                    </div>
                 </div>
             </div>
         </div>
