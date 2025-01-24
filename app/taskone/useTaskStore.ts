2c28fab3441c28c29ffc4d64402d99a5043a2e51
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-24 16:11:01 +0530

    added habits

--- app/taskone/useTaskStore.ts
+++ app/taskone/useTaskStore.ts
@@ -1,11 +1,22 @@
 import {create} from "zustand";
-
-interface WindowState{
+interface Habit{
+    name: string,
+    checked: boolean,
+}
+interface TaskStore{
+    habits: Habit[],
     isPopUpOpen: boolean,
-    togglePopUp: ()=> void
+    togglePopUp: ()=> void,
+    addHabit: (name: string) => void
 }
 
-export const useTaskStore = create<WindowState>((set)=>({
+export const useTaskStore = create<TaskStore>((set)=>({
+    habits: [
+        { name: "Meditating", checked: true },
+        { name: "Reading Philosophy", checked: true },
+        { name: "Journaling", checked: false },
+    ],
     isPopUpOpen: false,
-    togglePopUp: () => set((state) => ({ isPopUpOpen: !state.isPopUpOpen }))
+    togglePopUp: () => set((state) => ({ isPopUpOpen: !state.isPopUpOpen })),
+    addHabit: (name) =>set((state) => ({habits: [...state.habits, { name, checked: false }],})),
 }));
