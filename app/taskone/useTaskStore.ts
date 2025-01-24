c92cf686ac34b49ac13688c3577f4c442a580880
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-24 16:41:32 +0530

    added togglehabit function to update state of whole Habit Element

--- app/taskone/useTaskStore.ts
+++ app/taskone/useTaskStore.ts
@@ -7,16 +7,26 @@ interface TaskStore{
     habits: Habit[],
     isPopUpOpen: boolean,
     togglePopUp: ()=> void,
-    addHabit: (name: string) => void
+    addHabit: (name: string) => void,
+    toggleHabit: (index: number)=>void
 }
 
-export const useTaskStore = create<TaskStore>((set)=>({
+export const useTaskStore = create<TaskStore>((set) => ({
     habits: [
-        { name: "Meditating", checked: true },
-        { name: "Reading Philosophy", checked: true },
-        { name: "Journaling", checked: false },
+      { name: "Meditating", checked: true },
+      { name: "Reading Philosophy", checked: true },
+      { name: "Journaling", checked: false },
     ],
     isPopUpOpen: false,
     togglePopUp: () => set((state) => ({ isPopUpOpen: !state.isPopUpOpen })),
-    addHabit: (name) =>set((state) => ({habits: [...state.habits, { name, checked: false }],})),
-}));
+    addHabit: (name) =>
+      set((state) => ({
+        habits: [...state.habits, { name, checked: false }],
+      })),
+    toggleHabit: (index: number) =>
+      set((state) => {
+        const updatedHabits = [...state.habits];
+        updatedHabits[index].checked = !updatedHabits[index].checked;
+        return { habits: updatedHabits };
+      }),
+}));  
