69f28a27b51b2cc58df295aa762d29610d03888d
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-24 16:01:49 +0530

    converted input to required

--- app/taskone/page.tsx
+++ app/taskone/page.tsx
@@ -54,6 +54,7 @@ export default function TaskOne(){
                                     name="habit"
                                     className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md"
                                     placeholder=""
+                                    required
                                 />
                             </div>
                             <button
