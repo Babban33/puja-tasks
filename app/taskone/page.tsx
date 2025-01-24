fecfd54cd1f600a94d5a924cbd5d0805d01b6be2
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-24 15:23:12 +0530

    added form and save button

--- app/taskone/page.tsx
+++ app/taskone/page.tsx
@@ -59,6 +59,27 @@ export default function TaskOne(){
                                 </svg>
                             </button>
                         </div>
+
+                        <form className="space-y-4">
+                            <div>
+                                <label htmlFor="habit" className="block text-gray-700">
+                                Habit Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="habit"
+                                    name="habit"
+                                    className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md"
+                                    placeholder=""
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full py-2 px-4 text-white rounded-md bg-gradient-to-br from-orange-500 to-orange-400"
+                            >
+                                Save
+                            </button>
+                        </form>
                     </div>
                 </div>
             )}
