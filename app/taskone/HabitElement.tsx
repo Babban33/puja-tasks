dedb5381ac6adb4951c82c10d3c16b9fdec8b729
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-25 16:40:04 +0530

    removed index prop

--- app/taskone/HabitElement.tsx
+++ app/taskone/HabitElement.tsx
@@ -1,27 +1,22 @@
 import { MoreVertical } from "lucide-react";
-import { useTaskStore } from "./useTaskStore";
-interface HabitProps {
-    name: string;
-    checked: boolean;
-    index: number;
+
+interface HabitProps{
+    name: string,
+    checked: boolean,
 }
-  
-export default function HabitElement({ name, checked, index }: HabitProps) {
-    return (
-        <div
-            className={`flex justify-between ${checked ? "bg-gray-200" : "bg-gray-100"} p-4 items-center rounded-lg`}
-        >
+
+export default async function HabitElement({name, checked}: HabitProps){
+    return(
+        <div className={`flex justify-between ${checked ? "bg-gray-200" : "bg-gray-100"} p-4 items-center rounded-lg`}>
             <span className={checked ? "text-green-500" : ""}>{name}</span>
             <div className="flex items-center gap-2">
-            <input
-                type="checkbox"
-                className="form-checkbox size-6 text-emerald-400 rounded-md border-2 border-gray-400 focus:ring-0 cursor-pointer"
-                checked={checked}
-                onChange={() => useTaskStore.getState().toggleHabit(index)}
-            />
-            <MoreVertical className="size-4 cursor-pointer" />
+                <input
+                    type="checkbox"
+                    className="form-checkbox size-6 text-emerald-400 rounded-md border-2 border-gray-400 focus:ring-0 cursor-pointer"
+                    checked={checked}
+                />
+                <MoreVertical className="size-4 cursor-pointer" />
             </div>
         </div>
-    );
+    )
 }
-  
