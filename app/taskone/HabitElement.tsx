e2a3fe177c334f896c0b01bc3b4dda776b4b8000
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-25 17:43:13 +0530

    added id param to updated checked value

--- app/taskone/HabitElement.tsx
+++ app/taskone/HabitElement.tsx
@@ -1,11 +1,17 @@
+"use client";
 import { MoreVertical } from "lucide-react";
+import { toggleHabit } from "../actions";
 
 interface HabitProps{
+    id: string,
     name: string,
     checked: boolean,
 }
 
-export default async function HabitElement({name, checked}: HabitProps){
+export default function HabitElement({id, name, checked}: HabitProps){
+    const handleToggle = async () => {
+        await toggleHabit(id, checked);
+    };
     return(
         <div className={`flex justify-between ${checked ? "bg-gray-200" : "bg-gray-100"} p-4 items-center rounded-lg`}>
             <span className={checked ? "text-green-500" : ""}>{name}</span>
@@ -14,6 +20,7 @@ export default async function HabitElement({name, checked}: HabitProps){
                     type="checkbox"
                     className="form-checkbox size-6 text-emerald-400 rounded-md border-2 border-gray-400 focus:ring-0 cursor-pointer"
                     checked={checked}
+                    onChange={handleToggle}
                 />
                 <MoreVertical className="size-4 cursor-pointer" />
             </div>
